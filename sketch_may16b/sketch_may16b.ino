2d3d1062410397a198bde025f3f60e45b72aa6c2
jiafish <39110691+jiafish@users.noreply.github.com>
2018-05-16 11:39:52 +0800

    Add files via upload

---
+++ sketch_may16b/sketch_may16b.ino
@@ -0,0 +1,31 @@
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  13;
+// variables will change:
+
+void setup() {
+  // initialize the LED pin as an output:
+ 
+   pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+ int buttonState = digitalRead(buttonPin);
+Serial.println(buttonState);
+delay(1);
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+     
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off: 
+    digitalWrite(ledPin, LOW);
+   
+  
+  }
+}
